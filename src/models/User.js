3f1f808b32bdde356156af84e949675465e94e77
Thomas Boos Pegler <tboospegler@gmail.com>
2022-12-06 17:10:36 -0400

    Feat: add User model

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    underscored: true,
+    timestamps: false,
+    tableName: 'users',
+  });
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'user' });
+  };
+  return User;
+}
