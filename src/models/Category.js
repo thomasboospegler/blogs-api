c64bb6c1165f10c3f7900e38eaa982d100faf4b3
Thomas Boos Pegler <tboospegler@gmail.com>
2022-12-07 11:11:15 -0400

    Feat: add category model

---
+++ src/models/Category.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  }, {
+    underscored: true,
+    timestamps: false,
+  });
+  return Category;
+};
